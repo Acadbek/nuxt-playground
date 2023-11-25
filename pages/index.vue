72341822c71f81bb6cf6489c31790fccfab20859
Asadbek <ibnnumon@gmail.com>
2023-11-25 05:11:08 +0500

    chore: basic modules setup

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+const color = useColorMode()
+
+const changeMode = () => {
+    color.value = color.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+    <div class="text-red">
+        Hello {{ x }} {{ y }}
+        
+        <button @click="changeMode">{{ $colorMode.value }}</button>
+    </div>
+</template>
+
+
+<style scoped></style>
